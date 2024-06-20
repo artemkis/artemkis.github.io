6cbfa9ebdc9480a30424ccdc95bce9f8a9a848d4
Artem <artemkis2015@gmail.com>
2024-06-20 11:22:51 +0500

    Update main.js

--- main.js
+++ main.js
@@ -12,7 +12,7 @@
                 .parents()
                 .attr("data-percent") + "%"
           },
-          5000,
+          500,
           "linear",
           function() {
             return $(this).css({
@@ -41,7 +41,7 @@
         {
           strokeDashoffset: cbar
         },
-        5000,
+        500,
         "linear",
         function() {
           return circle.css({
